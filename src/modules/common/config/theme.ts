e7b38b06fb073b918c1f174148d361ac46337363
Guillermo Macchi <guillemacchi01@gmail.com>
2025-03-06 18:09:45 -0300

    chore: setup material ui next

---
+++ src/modules/common/config/theme.ts
@@ -0,0 +1,14 @@
+import { createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+  typography: {
+    fontFamily: "var(--font-roboto)",
+  },
+  palette: {
+    background: {
+      default: "#fafafa",
+    },
+  },
+});
+
+export default theme;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,24 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { AppCacheProvider } from "@mui/material-nextjs/v15-pagesRouter";
+import { ThemeProvider } from "@mui/material";
+import theme from "@/modules/common/config/theme";
+import { Roboto } from "next/font/google";
+const roboto = Roboto({
+  weight: ["300", "400", "500", "700"],
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-roboto",
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <AppCacheProvider>
+      <ThemeProvider theme={theme}>
+        <div id="root" className={roboto.className}>
+          <Component {...pageProps} />
+        </div>
+      </ThemeProvider>
+    </AppCacheProvider>
+  );
 }
